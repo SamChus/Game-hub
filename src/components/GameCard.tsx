c1b20ada223de951ad91e6849a17ba589b7a46ad
Samuel <samuelchigo55@gmail.com>
2024-01-06 18:47:01 +0100

    Get optimized image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import { Game } from '../hooks/useGame'
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList';
-import CriticScore from './CriticScore';
-
-
-
+import React from "react";
+import { Game } from "../hooks/useGame";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = ({game} :Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-        <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
-            <CardBody>
-                <Heading fontSize='2xl' >
-                    {game.name}
-                </Heading>
-                <HStack justifyContent={"space-between"} paddingTop={2}>
-                    <PlatformIconList platform={game.parent_platforms.map(p => p.platform)}/>
-                    <CriticScore  score={game.metacritic}/>
-                </HStack>
-            </CardBody>
-        </Card>
-    )
-}
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent={"space-between"} paddingTop={2}>
+          <PlatformIconList
+            platform={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
 
-export default GameCard
+export default GameCard;
