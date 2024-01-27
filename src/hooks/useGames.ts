d7a8af2a40694c1ad0d6bc49bcbe54b55d4aa7e1
Samuel <samuelchigo55@gmail.com>
2024-01-28 00:55:51 +0100

    Remove duplicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { QueryObject } from "../App";
-import { FetchResponse } from "../services/api-client";
-import { Genre } from "./useGenres";
-import apiClient from "../services/api-client";
+import apiClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+
 
 export interface Game {
   id: number;
