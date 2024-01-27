0b18ee8d9f670d502157459d7952fd03326ecb32
Samuel <samuelchigo55@gmail.com>
2024-01-27 23:52:07 +0100

    refractor platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -5,15 +5,20 @@ import apiClient from "../services/api-client";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatform = () => useQuery({
+const usePlatform = () =>
+  useQuery({
     queryKey: ["platforms"],
-    queryFn: ()=>apiClient.get<fetchResponse<Platform>>("/platforms").then((res) => res.data),
-});
-
+    queryFn: () =>
+      apiClient
+        .get<fetchResponse<Platform>>("/platforms")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatform;
