2f02be5172da1b6c793fb9bc65e57f734309a45a
Samuel <samuelchigo55@gmail.com>
2024-01-30 13:14:09 +0100

    Refractoring- create a reuseable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { QueryObject } from "../App";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse,  } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 
+const apiClient = new APIClient<Game>('/games')
 
 export interface Game {
   id: number;
@@ -17,18 +18,13 @@ export interface Game {
 const useGame = (gameQuery: QueryObject) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
-          params: {
-            genre: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1000,
-    // initialData: { count: 0, results: [] },
+    queryFn: () => apiClient.getAll({
+      params: {
+        genre: gameQuery.genre?.id,
+        parent_platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
+      }
+    }),
   });
 export default useGame;
