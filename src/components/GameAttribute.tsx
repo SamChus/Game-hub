e6699688b8794a1d3949cc82d973e221b41a0b00
Samuel <samuelchigo55@gmail.com>
2024-02-27 00:11:33 +0100

    Adding Game Attribute

---
+++ src/components/GameAttribute.tsx
@@ -0,0 +1,36 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import React from 'react'
+import CriticScore from './CriticScore';
+import DefinitionText from './DefinitionText';
+import { Game } from '../entities/Game';
+
+interface Props{
+    game: Game
+}
+
+const GameAttribute = ({game}:Props) => {
+  return (
+    <SimpleGrid columns={2}>
+      <DefinitionText term="Platform">
+        {game.parent_platforms.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinitionText>
+      <DefinitionText term="Meta Score">
+        <CriticScore score={game.metacritic} key={game.id} />
+      </DefinitionText>
+      <DefinitionText term="Meta Score">
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefinitionText>
+      <DefinitionText term="Meta Score">
+        {game.publishers?.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefinitionText>
+    </SimpleGrid>
+  );
+}
+
+export default GameAttribute
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Button, Heading, Spinner, Text} from "@chakra-ui/react";
+import { Button, Heading, SimpleGrid, Spinner, Text} from "@chakra-ui/react";
 import { useState } from "react";
 import ExpandableText from "../components/ExpandableText";
 import DefinitionText from "../components/DefinitionText";
 import { platform } from "os";
+import CriticScore from "../components/CriticScore";
+import GameAttribute from "../components/GameAttribute";
 
 const GameDetailPage = () => {
   const [showMore, setShowMore] = useState(false);
@@ -26,12 +28,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <ExpandableText>
-        {game.description_raw}
-      </ExpandableText>
-      <DefinitionText term="Platform">
-        {game.parent_platforms.map(({platform})=> <Text>{platform.name}</Text>)}
-      </DefinitionText>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+     <GameAttribute  game={game}/>
     </>
   );
 };
