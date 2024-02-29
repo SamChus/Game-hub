c30594f01b6ca9eecf391228ba1f7f3401ad5b54
Samuel <samuelchigo55@gmail.com>
2024-02-29 07:48:48 +0100

    Fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,16 @@ import logo from "../assets/logo.webp";
 import React from "react";
 import ColoeModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize={20} />
+      <Link to="/" >
+        <Image src={logo} boxSize="60px" objectFit="cover"/>
+      </Link>
       <SearchInput />
       <ColoeModeSwitch />
     </HStack>
